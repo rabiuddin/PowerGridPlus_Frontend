91e3024dd1ba7175b95b4f093ecc4a5e5f525545
HuzaifaRizwan1231 <huzaifa.rizwan1231@gmail.com>
2025-02-23 17:55:55 +0500

    Add LatestNews component to display news items in a grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
+import LatestNews from "./components/LatestNews";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <HeroSection/>
+      <LatestNews/>
       <Footer />
     </>
   );
