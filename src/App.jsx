f354033631fa972396e7c01a928e5ebd8e85ce7b
rabiuddin <rabiuddin1@gmail.com>
2025-02-23 23:43:02 +0500

    Refactor App structure by creating Home page component and organizing imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import HeroSection from "./components/HeroSection";
-import LatestNews from "./components/LatestNews";
-import CallToAction from "./components/CallToAction";
-import WhyChooseUs from "./components/WhyChooseUs";
-import FAQ from "./components/FAQ";
-import HowItWorks from "./components/HowItWorks";
+import Navbar from "./components/Shared/Navbar";
+import Footer from "./components/Shared/Footer";
+import Home from "./pages/Home/Home";
 
 const App = () => {
   return (
     <>
       <Navbar />
-
-      <HeroSection/>
-      <WhyChooseUs />
-      <HowItWorks />
-      <LatestNews/>
-      <FAQ />
-      <CallToAction/>
       
+      <Home />
+
       <Footer />
     </>
   );
