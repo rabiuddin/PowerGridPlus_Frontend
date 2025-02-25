9090b84329b0cfc89f57b74ae949a6c0bae0efac
rabiuddin <rabiuddin1@gmail.com>
2025-02-25 15:18:26 +0500

    Refactor layout structure by removing Navbar and Footer from App component; introduce MainLayout for consistent layout management across pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import Navbar from "./components/shared/Navbar";
-import Footer from "./components/shared/Footer";
 import Home from "./pages/Home"
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      
+
       <Home />
 
-      <Footer />
     </>
   );
 };
